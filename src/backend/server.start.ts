4bd4d32f6e3d538f263acec1c5e48658e76a7e98
r4kogama <thecureomd@gmail.com>
2024-07-23 03:23:14 +0200

    routes express http

--- src/backend/server.start.ts
+++ src/backend/server.start.ts
@@ -1,10 +1,22 @@
 import { App } from "./App";
-
+import { controlInjection } from "../task/infrastructure/dependency/injectionControl";
+import { landingpage } from "../views/layout/main";
 
 const app: App = new App();
 app.start().then( () =>{
 	try {
-	
+		const appExpress = app.server?.getExpress();
+		if(!appExpress){
+			throw new Error ("Error starting express server");
+		}
+
+		const control = controlInjection();
+		appExpress.get('/',(req, res ) =>{res.send(landingpage)});
+		appExpress.get('/tasks/:id', control.getTaskIdController);
+		appExpress.get('/tasks', control.getAllTasksController);
+		appExpress.post('/tasks', control.postCreateTaskController);
+		appExpress.patch('/tasks/:id', control.patchTaskStatusController);
+		appExpress.delete('/tasks/:id', control.deleteTaskController);
 	} catch (e) {
 		console.log(e);
 		process.exit(1);
@@ -13,4 +25,3 @@ app.start().then( () =>{
 process.on("uncaughtException", () => {
 	process.exit(1);
 });
-
