ae7c536c8bc1685818231c959e5462b9296a3c18
r4kogama <thecureomd@gmail.com>
2024-07-23 03:18:52 +0200

    injection dependency control

--- src/task/infrastructure/dependency/injectionControl.ts
+++ src/task/infrastructure/dependency/injectionControl.ts
@@ -1,4 +1,12 @@
+import { TaskController } from "../controller/taskHttpController";
+import { ManageTask } from "../adapters/manageTask";
+import { HttpResponse } from "../../../shared/response/httpResponse";
+import { TaskServiceInteractor } from "../../application/services/taskService";
 
-
-
-
+export const controlInjection = ():TaskController => {
+    const statusRepository : HttpResponse = new HttpResponse();
+    const manageTask: ManageTask = new ManageTask();
+    const taskService: TaskServiceInteractor = new TaskServiceInteractor(manageTask);
+    const taskControl: TaskController = new TaskController(taskService, statusRepository);
+    return taskControl;
+}
