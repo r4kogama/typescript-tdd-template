5d50c25bc6c2adfa746a87ef3ccc129c9785d473
r4kogama <thecureomd@gmail.com>
2024-07-24 03:35:35 +0200

    middelware authorization done

--- src/backend/middlewares/auth.ts
+++ src/backend/middlewares/auth.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { HttpResponse } from '../../shared/response/httpResponse';
 
-const authMiddelware = (req: Request, res: Response, next: NextFunction)=>{
+export const authMiddelware = (req: Request, res: Response, next: NextFunction)=>{
     const header: string | undefined = req.headers['authorization'];
     const data: Record<string, string> = {message: "No authorization"}
     const statusRepository : HttpResponse = new HttpResponse();
     if(!header){
        return statusRepository.statusForbidden(res, data )
     }
-    const credentials = header.split(' ');
-    console.log(credentials)
-}
+    //const credentials:string = Buffer.from('admin:admin').toString('base64');
+    const credentials: string = header.split(' ')[1];
+    console.log(credentials);
+    if(!credentials){
+        return statusRepository.statusForbidden(res, data )
+    }
 
-module.exports = authMiddelware;
+    const decoCredentias: string = Buffer.from(credentials, 'base64').toString('utf-8');
+    const [user, password] = decoCredentias.split(':');
+    console.log(decoCredentias);
+    if(user === 'admin' && password === 'admin'){
+        return next();
+    }
+    return statusRepository.statusForbidden(res, data )
+}
--- src/task/infrastructure/routes/route.ts
+++ src/task/infrastructure/routes/route.ts
@@ -1,22 +1,22 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { landingpage } from '../../../views/layout/main';
 import { controlInjection } from '../dependency/injectionControl';
-
+import { authMiddelware } from '../../../backend/middlewares/auth';
 
 const control = controlInjection();
 const router = express.Router();
 
 export const routes = () => {
     router.get('/',(req: Request, res:Response ) =>{res.send(landingpage)});
-    router.get('/:id',   control.getTaskIdController);
-    router.post('/',     control.postCreateTaskController);
-    router.patch('/:id', control.patchTaskStatusController);
-    router.delete('/:id',control.deleteTaskController);
+    router.get('/:id',    authMiddelware,  control.getTaskIdController);
+    router.post('/',      control.postCreateTaskController);
+    router.patch('/:id',  authMiddelware, control.patchTaskStatusController);
+    router.delete('/:id', authMiddelware, control.deleteTaskController);
     return router;
 }
 
 export const routeSpecific = () => {
-    router.get('/',control.getAllTasksController);
+    router.get('/', authMiddelware, control.getAllTasksController);
     return router;
 }
 
